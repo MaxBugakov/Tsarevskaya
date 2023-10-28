5d9f210070bd4f15f80bda2371681476a8a784e8
Max Bugakov <max.bugakov18@gmail.com>
2023-10-28 11:02:05 +0300

    com

--- form-page/main.js
+++ form-page/main.js
@@ -1,5 +1,16 @@
 $(document).ready( function(){
-    $("#send-button").on("click", function() {
-        console.log("Sended");
+    // $("#send-button").on("click", function() {
+    //     console.log("Sended");
+    // });
+    let send_button_click_flag=false;
+    $("#send-button").click(function(e) {
+        e.preventDefault();
+        console.log("Hello!");
+        if (send_button_click_flag==false) {
+            $('.form-title').addClass('eror-form');
+            $('.form').addClass('eror-form');
+            $('.send-message-wrapper').removeClass('eror-form');
+            send_button_click_flag=true;
+        }
     });
 });
